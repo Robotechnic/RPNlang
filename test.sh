a0bbd171fb095eb7a48b3feeb4238b88d671a16a
Robotechnic <robotechnic.developeur@gmail.com>
2022-06-11 19:33:39 +0200

    add some more tests

--- test.sh
+++ test.sh
@@ -2,12 +2,15 @@
 
 for example in ./tests/*
 do
+	echo "============================================================"
 	echo "Testing $example"
+	echo "============================================================"
 	./build/RPNlang "$example"
 	if [ $? -ne 0 ]
 	then
 		echo "Test $example failed"
 	fi
+	echo
 done
 
 echo "Done"
