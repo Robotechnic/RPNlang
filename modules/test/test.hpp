ff52f4c2166aa1aad5906efeb286a9085098f850
Robotechnic <robotechnic.developeur@gmail.com>
2022-12-04 19:41:07 +0100

    add forgoten include

--- modules/test/test.hpp
+++ modules/test/test.hpp
@@ -1,3 +1,7 @@
+#pragma once
 #include "expressionresult/expressionresult.hpp"
 #include "cppmodule/cppmodule.hpp"
+#include "value/types/string.hpp"
+#include "value/types/none.hpp"
+
 ExpressionResult loader(CppModule &module);
